07b6f49b8361334f5082edfdbe083d62123e9dca
TawfikBelaifa <tawfik.professionnel@gmail.com>
2021-07-29 19:58:24 +0200

    part accueil V2.5.2

--- Vue.js
+++ Vue.js
@@ -1,48 +1,427 @@
 const API = [
     {
-        id:1, 
-        producName:"CHRISTIAN LOUBOUTIN", 
-        producS_Name:"Morbi euismod molestie dolor, sed", 
-        description:"A signature style of the brand Louboutin come in dark grey", 
-        price:"750", 
-        img:"1.jpg", 
-        cat:"Men", 
-        type:"shoes"
+        "id": "1",
+        "color": "Goldenrod",
+        "description": "Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.",
+        "gender": "Male",
+        "name": "Desi Avramovitz",
+        "review": "productize virtual markets",
+        "starrating": 3,
+        "price": 50.4,
+        "img": "1.jpg"
     },
     {
-        id:2, 
-        producName:"GUCCI", 
-        producS_Name:"Nulla massa felis, venenatis elementum.", 
-        description:"A signature style of the brand, these Lou Spikes Orlato sneaker", 
-        price:"550",
-        img:"2.jpg", 
-        cat:"Men", 
-        type:"shoes"
+        "id": "2",
+        "color": "Puce",
+        "description": "Phasellus in felis. Donec semper sapien a libero. Nam dui.",
+        "gender": "Male",
+        "name": "Addy Alldre",
+        "review": "deploy efficient mindshare",
+        "starrating": 4,
+        "price": 33.99,
+        "img": "2.jpg"
     },
     {
-        id:2, 
-        producName:"BALENCIAGA", 
-        producS_Name:"Duis cursus lectus quis ullamcorper.", 
-        description:"A signature style of the brand, these Lou Spik", 
-        price:"775",
-        img:"2.jpg", 
-        cat:"Men", 
-        type:"shoes"
+        "id": "3",
+        "color": "Orange",
+        "description": "Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.",
+        "gender": "Female",
+        "name": "Bernie Gledhill",
+        "review": "mesh sticky content",
+        "starrating": 3,
+        "price": 102.99,
+        "img": "3.jpg"
     },
+    {
+        "id": "4",
+        "color": "Teal",
+        "description": "Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.\n\nMorbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.",
+        "gender": "Female",
+        "name": "Gwendolen Bickerstaffe",
+        "review": "integrate front-end infomediaries",
+        "starrating": 5,
+        "price": 64.5,
+        "sizes": [
+            "Small",
+            "Medium",
+            "Large"
+        ],
+        "img": "4.jpg"
+    },
+    {
+        "id": "5",
+        "color": "Yellow",
+        "description": "Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.\n\nPraesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.\n\nCras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+        "gender": "Male",
+        "name": "Cristian Gilbanks",
+        "review": "integrate extensible methodologies",
+        "starrating": 5,
+        "price": 30.99,
+        "img": "5.png"
+    },
+    {
+        "id": "6",
+        "color": "Red",
+        "description": "Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.\n\nDuis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.\n\nIn sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.",
+        "gender": "Female",
+        "name": "Kalila Gooms",
+        "review": "utilize end-to-end functionalities",
+        "starrating": 4,
+        "price": 19.99,
+        "sizes": [
+            "Small",
+            "Medium",
+            "Large"
+        ],
+        "img": "6.png"
+    },
+    {
+        "id": "7",
+        "color": "Maroon",
+        "description": "Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.\n\nCras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.\n\nQuisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.",
+        "gender": "Male",
+        "name": "Bartolemo Peckitt",
+        "review": "enable cutting-edge e-services",
+        "starrating": 3,
+        "price": 130.0,
+        "sizes": [
+            "Small",
+            "Medium",
+            "Large"
+        ],
+        "img": "7.png"
+    },
+    {
+        "id": "8",
+        "color": "Puce",
+        "description": "Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.",
+        "gender": "Female",
+        "name": "Kathye Haith",
+        "review": "innovate sexy portals",
+        "starrating": 4,
+        "price": 120.99,
+        "sizes": [
+            "Small",
+            "Medium",
+            "Large"
+        ],
+        "img": "8.png"
+    },
+    {
+        "id": "9",
+        "color": "Red",
+        "description": "Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.\n\nPraesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.\n\nMorbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.",
+        "gender": "Male",
+        "name": "Armin Basilio",
+        "review": "transform robust mindshare",
+        "starrating": 5,
+        "price": 20.99,
+        "sizes": [
+            "Small",
+            "Medium",
+            "Large"
+        ],
+        "img": "9.png"
+    },
+    {
+        "id": "10",
+        "color": "Fuscia",
+        "description": "Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.\n\nProin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.\n\nDuis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.",
+        "gender": "Male",
+        "name": "Cal Sterman",
+        "review": "benchmark synergistic bandwidth",
+        "starrating": 3,
+        "price": 50.0,
+        "img": "10.jpg"
+    },
+    {
+        "id": "11",
+        "color": "Mauv",
+        "description": "Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.",
+        "gender": "Female",
+        "name": "Ardine Imlaw",
+        "review": "repurpose robust e-business",
+        "starrating": 4,
+        "price": 65.0,
+        "sizes": [
+            "Small",
+            "Medium",
+            "Large"
+        ],
+        "img": "11.png"
+    },
+    {
+        "id": "12",
+        "color": "Teal",
+        "description": "Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.",
+        "gender": "Female",
+        "name": "Noella Ruddom",
+        "review": "redefine 24/365 eyeballs",
+        "starrating": 3,
+        "price": 72.0,
+        "sizes": [
+            "Small",
+            "Medium",
+            "Large"
+        ],
+        "img": "12.png"
+    },
+    {
+        "id": "13",
+        "color": "Khaki",
+        "description": "Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.\n\nCurabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.",
+        "gender": "Female",
+        "name": "Kirstyn Espadate",
+        "review": "empower sticky web-readiness",
+        "starrating": 4,
+        "price": 59.99,
+        "img": "13.png"
+    },
+    {
+        "id": "14",
+        "color": "Turquoise",
+        "description": "Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.",
+        "gender": "Male",
+        "name": "Rourke Greet",
+        "review": "deliver end-to-end solutions",
+        "starrating": 5,
+        "price": 49.99,
+        "img": "14.png"
+    },
+    {
+        "id": "15",
+        "color": "Teal",
+        "description": "Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.\n\nPhasellus in felis. Donec semper sapien a libero. Nam dui.",
+        "gender": "Female",
+        "name": "Arden Stockbridge",
+        "review": "orchestrate value-added infrastructures",
+        "starrating": 4,
+        "price": 100.0,
+        "img": "15.png"
+    },
+    {
+        "id": "16",
+        "color": "Orange",
+        "description": "Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.",
+        "gender": "Male",
+        "name": "Sheffy Gunter",
+        "review": "revolutionize vertical systems",
+        "starrating": 5,
+        "price": 33.99,
+        "img": "16.jpg"
+    },
+    {
+        "id": "17",
+        "color": "Orange",
+        "description": "Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.\n\nMorbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.",
+        "gender": "Female",
+        "name": "Alexine Mulligan",
+        "review": "architect real-time partnerships",
+        "starrating": 3,
+        "price": 13.99,
+        "sizes": [
+            "Small",
+            "Medium",
+            "Large"
+        ],
+        "img": "17.png"
+    },
+    {
+        "id": "18",
+        "color": "Purple",
+        "description": "In congue. Etiam justo. Etiam pretium iaculis justo.\n\nIn hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.\n\nNulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.",
+        "gender": "Female",
+        "name": "Micheline Charlson",
+        "review": "innovate holistic markets",
+        "starrating": 4,
+        "price": 12.99,
+        "img": "18.jpg"
+    },
+    {
+        "id": "19",
+        "color": "Purple",
+        "description": "Phasellus in felis. Donec semper sapien a libero. Nam dui.\n\nProin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.\n\nInteger ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.",
+        "gender": "Male",
+        "name": "Hughie Jeffryes",
+        "review": "facilitate innovative portals",
+        "starrating": 3,
+        "price": 154.99,
+        "sizes": [
+            "Small",
+            "Medium",
+            "Large"
+        ],
+        "img": "19.png"
+    },
+    {
+        "id": "20",
+        "color": "Puce",
+        "description": "In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.\n\nNulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.",
+        "gender": "Male",
+        "name": "Chet Corke",
+        "review": "unleash synergistic models",
+        "starrating": 5,
+        "price": 33.99,
+        "img": "1.jpg"
+    },
+    {
+        "id": "21",
+        "color": "Orange",
+        "description": "Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.\n\nPhasellus in felis. Donec semper sapien a libero. Nam dui.",
+        "gender": "Female",
+        "name": "Kamila Yggo",
+        "review": "unleash strategic platforms",
+        "starrating": 4,
+        "price": 50.99,
+        "img": "2.jpg"
+    },
+    {
+        "id": "22",
+        "color": "Purple",
+        "description": "Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.\n\nVestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.",
+        "gender": "Female",
+        "name": "Kandace Strickett",
+        "review": "orchestrate visionary paradigms",
+        "starrating": 4,
+        "price": 50.99,
+        "img": "3.jpg"
+    },
+    {
+        "id": "23",
+        "color": "Khaki",
+        "description": "Phasellus in felis. Donec semper sapien a libero. Nam dui.",
+        "gender": "Male",
+        "name": "Kellby Arlott",
+        "review": "enhance rich initiatives",
+        "starrating": 4,
+        "price": 19.99,
+        "sizes": [
+            "Small",
+            "Medium",
+            "Large"
+        ],
+        "img": "4.jpg"
+    },
+    {
+        "id": "24",
+        "color": "Purple",
+        "description": "Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.\n\nPraesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.\n\nMorbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.",
+        "gender": "Male",
+        "name": "Bogey Cheyne",
+        "review": "benchmark mission-critical convergence",
+        "starrating": 5,
+        "price": 110.0,
+        "img": "5.png"
+    },
+    {
+        "id": "25",
+        "color": "Crimson",
+        "description": "Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.\n\nNullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.\n\nMorbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.",
+        "gender": "Female",
+        "name": "Sybila Ehlerding",
+        "review": "aggregate granular systems",
+        "starrating": 3,
+        "price": 19.99,
+        "sizes": [
+            "Small",
+            "Medium",
+            "Large"
+        ],
+        "img": "6.png"
+    },
+    {
+        "id": "26",
+        "color": "Purple",
+        "description": "Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.\n\nPhasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.\n\nProin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.",
+        "gender": "Male",
+        "name": "Hadrian Hambatch",
+        "review": "transition granular markets",
+        "starrating": 3,
+        "price": 190.99,
+        "sizes": [
+            "Small",
+            "Medium",
+            "Large"
+        ],
+        "img": "7.png"
+    },
+    {
+        "id": "27",
+        "color": "Orange",
+        "description": "Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.\n\nDuis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.\n\nDonec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.",
+        "gender": "Female",
+        "name": "Franciska Hallett",
+        "review": "generate customized e-services",
+        "starrating": 4,
+        "price": 20.0,
+        "sizes": [
+            "Small",
+            "Medium",
+            "Large"
+        ],
+        "img": "8.png"
+    },
+    {
+        "id": "28",
+        "color": "Orange",
+        "description": "Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+        "gender": "Male",
+        "name": "Andrej Wilfing",
+        "review": "e-enable viral web services",
+        "starrating": 3,
+        "price": 15.99,
+        "sizes": [
+            "Small",
+            "Medium",
+            "Large"
+        ],
+        "img": "9.png"
+    },
+    {
+        "id": "29",
+        "color": "Aquamarine",
+        "description": "Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.\n\nPraesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.\n\nMorbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.",
+        "gender": "Female",
+        "name": "Rori Marishenko",
+        "review": "recontextualize B2B markets",
+        "starrating": 3,
+        "price": 190.0,
+        "img": "10.jpg"
+    },
+    {
+        "id": "30",
+        "color": "Pink",
+        "description": "Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.",
+        "gender": "Male",
+        "name": "Silas Cornwell",
+        "review": "architect collaborative initiatives",
+        "starrating": 3,
+        "price": 35.99,
+        "sizes": [
+            "Small",
+            "Medium",
+            "Large"
+        ],
+        "img": "11.png"
+    }
 ]
 
+
 const Accueil = {
-    template: '#accueil', 
+    template: '#accueil',
     name: 'Accueil',
-    data: () => {
+    data() {
         return {
-            Api: API,
+            dataUse: [],
         }
+    },
+    mounted() {
+        this.dataUse = API.filter((API) => API.id <= 3)
     }
 }
 
 const Tous = {
-    template: '#tous', 
+    template: '#tous',
     name: 'Tous',
     data: () => {
         return {
@@ -52,10 +431,10 @@ const Tous = {
 }
 
 const routes = [
-    {path: '/', component: Accueil},
-    {path: '/Tous', component: Tous},
+    { path: '/', component: Accueil },
+    { path: '/Tous', component: Tous },
 
-    
+
 ]
 
 const router = new VueRouter({
